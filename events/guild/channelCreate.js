30a23e475ad307fd8cd7d009b06899ff31bbc33d
Alban <alban@pop-os.localdomain>
2021-04-09 12:10:54 +0200

    create-delete-channel-event

---
+++ events/guild/channelCreate.js
@@ -0,0 +1,41 @@
+const { MessageEmbed } = require("discord.js");
+const { vert } = require("../../couleurs");
+
+module.exports = async (client, channel) => {
+  function type(chan) {
+    if (chan === "text") return "Texte";
+    if (chan === "voice") return "Vocale";
+    if (chan === "news") return "Annonce";
+  }
+  const fetchGuildAuditLogs = await channel.guild.fetchAuditLogs({
+    limit: 1,
+    type: "CHANNEL_CREATE",
+  });
+  const lastChannelCreated = fetchGuildAuditLogs.entries.first();
+  console.log(channel.type);
+  const { executor } = lastChannelCreated;
+
+  const embed = new MessageEmbed()
+    .setColor(vert)
+    .setAuthor(executor.username, executor.displayAvatarURL())
+    .setThumbnail(executor.displayAvatarURL({ dynamic: true }))
+    .setTitle(`Un salon vient d'être créer`)
+    .addField("Nom :", channel.name, true)
+
+    .addField("Type :", type(channel.type), true)
+
+    .addField("Position", channel.rawPosition, true)
+    .addField(`NSFW :`, channel.nsfw ? "Oui" : "Non", true)
+    .setTimestamp();
+  if (channel.parent) {
+    embed.addField("Categorie :", channel.parent.name, true);
+  }
+  if (channel.topic) {
+    embed.addField(`Topic`, channel.topic);
+  }
+
+  await client.guilds.cache
+    .get(channel.guild.id)
+    .channels.cache.get("830018869614870538")
+    .send(embed);
+};
---
+++ events/guild/channelDelete.js
@@ -0,0 +1,47 @@
+const { rouge } = require("../../couleurs");
+const { MessageEmbed } = require("discord.js");
+const moment = require("moment");
+module.exports = async (client, channel) => {
+  function type(chan) {
+    if (chan === "text") return "Texte";
+    if (chan === "voice") return "Vocale";
+    if (chan === "news") return "Annonce";
+  }
+  console.log(channel);
+
+  const fetchGuildAuditLogs = await channel.guild.fetchAuditLogs({
+    limit: 1,
+    type: "CHANNEL_DELETE",
+  });
+  const lastChannelDeleted = fetchGuildAuditLogs.entries.first();
+  console.log(lastChannelDeleted);
+  const { executor } = lastChannelDeleted;
+  const embed = new MessageEmbed()
+
+    .setColor(rouge)
+    .setTitle(`Un salon vient d'être supprimer`)
+    .setAuthor(executor.username, executor.displayAvatarURL())
+    .setThumbnail(executor.displayAvatarURL({ dynamic: true }))
+    .addField("Nom :", channel.name, true)
+    .addField("Type :", type(channel.type), true)
+    .addField(
+      `Créer le :`,
+      moment(channel.createdAt)
+        .locale("fr")
+        .format("dddd Do MMMM  YYYY, HH:mm:ss"),
+      true
+    )
+    .addField(`NSFW :`, channel.nsfw ? "Oui" : "Non", true)
+    .setTimestamp();
+
+  if (channel.parent) {
+    embed.addField("Categorie :", channel.parent.name, true);
+  }
+  if (channel.topic) {
+    embed.addField("Topic :", channel.topic);
+  }
+  await client.guilds.cache
+    .get(channel.guild.id)
+    .channels.cache.get("830018869614870538")
+    .send(embed);
+};
